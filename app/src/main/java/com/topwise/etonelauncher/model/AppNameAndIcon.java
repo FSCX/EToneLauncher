4ee1921909e9da72a674a4a5fc910641432584e7
fanshengcai <fanshengcai@topwisesz.com>
2018-05-25 17:48:10 +0800

    修改一个系统应用加载显示

--- app/src/main/java/com/topwise/etonelauncher/model/AppNameAndIcon.java
+++ app/src/main/java/com/topwise/etonelauncher/model/AppNameAndIcon.java
@@ -70,7 +70,7 @@ public class AppNameAndIcon {
         for (PackageInfo packgeInfo : packgeInfos) {
             //去除系统应用
             ApplicationInfo appInfo = packgeInfo.applicationInfo;
-            if (!filterApp(appInfo)) {
+            if (!filterApp(appInfo)&&!appInfo.packageName.equals("com.topwise.etone.demo")) {
                 continue;
             }
             /***
@@ -99,10 +99,10 @@ public class AppNameAndIcon {
     }
 
     //判断某一个应用程序是不是系统的应用程序，
-    //如果是返回true，否则返回false。
+    //如果是返回false，否则返回true。
     public boolean filterApp(ApplicationInfo info) {
     //有些系统应用是可以更新的，如果用户自己下载了一个系统的应用来更新了原来的，它还是系统应用，这个就是判断这种情况的
-        if ((info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+        if ((info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0 ) {
             return true;
             //判断是不是系统应用
         } else if ((info.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
